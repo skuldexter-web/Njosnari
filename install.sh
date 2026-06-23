8adbea99a1ebd7cd0f1da5d0e538602036bce9c4
SⱩUⱠÐ <skuldexter@gmail.com>
2026-06-23 21:17:50 +0200

    install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Maak dit bestand uitvoerbaar met: chmod +x install.sh
+echo "[*] NJÓSNARI Deployment initiated..."
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-tk metasploit-framework
+pip3 install -r requirements.txt --break-system-packages
+echo "[+] Dependencies installed. Ready for launch."
